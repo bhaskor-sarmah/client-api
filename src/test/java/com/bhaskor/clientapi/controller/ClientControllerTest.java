ff8b63931f267bce3b78fa221374a855e1086761
Bhaskor Jyoti Sarmah <bhaskor88@gmail.com>
2021-05-29 19:47:11 +0530

    Create New Client Test Completed

--- src/test/java/com/bhaskor/clientapi/controller/ClientControllerTest.java
+++ src/test/java/com/bhaskor/clientapi/controller/ClientControllerTest.java
@@ -1,46 +1,34 @@
 package com.bhaskor.clientapi.controller;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 import com.bhaskor.clientapi.entity.ClientEntity;
 import com.bhaskor.clientapi.payload.response.JsonResponse;
-import com.bhaskor.clientapi.repository.ClientRepository;
-import com.bhaskor.clientapi.service.ClientService;
+import com.google.gson.Gson;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.ResponseEntity;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
-@ExtendWith(MockitoExtension.class)
+@AutoConfigureMockMvc
 public class ClientControllerTest {
 
    @Autowired
-   @InjectMocks
-   ClientService clientService;
-
-   @Autowired
-   ClientController clientController;
-
-   @Mock
-   ClientRepository clientRepository;
+   MockMvc mvc;   
 
    @Test
-   public void testCreateClient() {
+   @DisplayName("Test - Creating a Client Integration Test")
+   public void testCreateClient() throws Exception {
       ClientEntity client = new ClientEntity();
       client.setId(1L);
       client.setFirstName("Bhaskor");
@@ -49,18 +37,46 @@ public class ClientControllerTest {
       client.setMobileNumber("+91-7002402636");
       client.setPhysicalAddress("My Address");
 
-      MockHttpServletRequest request = new MockHttpServletRequest();
-      RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+      Gson gson = new Gson();
+      String json = gson.toJson(client);
 
-      when(clientRepository.save(any(ClientEntity.class))).thenReturn(client);
-      BindingResult result = mock(BindingResult.class);
+      RequestBuilder request = MockMvcRequestBuilders.post("/api/1.0/createClient").contentType(MediaType.APPLICATION_JSON).content(json);
+      MvcResult result = mvc.perform(request).andReturn();
 
-      assertThat(clientController).isNotNull();
+      System.out.println();
 
-      ResponseEntity<JsonResponse> responseEntity = (ResponseEntity<JsonResponse>) clientController.createNewClient(client, result);
+      JsonResponse res = gson.fromJson(result.getResponse().getContentAsString(), JsonResponse.class);
 
-      assertEquals(200, responseEntity.getStatusCodeValue());
-      // System.out.println(responseEntity.getBody());
-      assertTrue(responseEntity.getBody().isStatus());
+      assertEquals(200, result.getResponse().getStatus());
+      assertTrue(res.isStatus());
+      assertEquals("Client Saved Successfully",res.getMessage());
+      
    }
+
+   @Test
+   public void testCreateClientInvalidIdNumber() throws Exception {
+      ClientEntity client = new ClientEntity();
+      client.setId(1L);
+      client.setFirstName("Bhaskor");
+      client.setLastName("Sarmah");
+      client.setIdNumber("840911 4567");
+      client.setMobileNumber("+91-7002402636");
+      client.setPhysicalAddress("My Address");
+
+      Gson gson = new Gson();
+      String json = gson.toJson(client);
+
+      RequestBuilder request = MockMvcRequestBuilders.post("/api/1.0/createClient").contentType(MediaType.APPLICATION_JSON).content(json);
+      MvcResult result = mvc.perform(request).andReturn();
+
+      System.out.println();
+
+      JsonResponse res = gson.fromJson(result.getResponse().getContentAsString(), JsonResponse.class);
+
+      assertEquals(400, result.getResponse().getStatus());
+      assertFalse(res.isStatus());
+      assertEquals("Data validation error",res.getMessage());
+      
+   }
+
 }
